a2700d717158348c1fdb27ee05f5986e3596dc4f
JL-OLEMAR <joseluis19963@gmail.com>
2022-03-28 19:52:12 -0500

    Add example useFetch

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import ReactDOM from 'react-dom'
 
-import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook.jsx'
+import { MultipleCustomHooks } from './components/03-exanples/MultipleCustomHooks.jsx'
 
 // import { CounterApp } from './components/01-useState/CounterApp.jsx'
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook.jsx'
 // import { SimpleForm } from './components/02-useEffect/SimpleForm.jsx'
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook.jsx'
 
 import './index.css'
 
 ReactDOM.render(
-  <FormWithCustomHook />,
+  <MultipleCustomHooks />,
   document.getElementById('root')
 )
